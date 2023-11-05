98dde7e531f2321bf9fb6c41692cd9b0ce9ad4a3
nachorm96 <rubio.nacho@yahoo.com.ar>
2023-11-04 23:19:09 -0300

    agrego estructura de archivos ft nodeJS & express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3030;
+
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','index.html')))
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views','login.html')))
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views','register.html')))
+app.get('/productCart', (req,res) => res.sendFile(path.join(__dirname, 'views','productCart.html')))
+app.get('/productDetail', (req,res) => res.sendFile(path.join(__dirname, 'views','productDetail.html')))
+
+
+app.listen(PORT,() => console.log(`Servidor corriendo en http://localhost:${PORT} `))
---
+++ retro.md
@@ -0,0 +1,2 @@
+### Retrospectiva Sprint 1
+---
