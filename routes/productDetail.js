3f5a4db8ce041634b28a0b73ef3144130f21a156
nachorm96 <rubio.nacho@yahoo.com.ar>
2023-12-06 13:44:40 -0300

    pckg json

--- routes/productDetail.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET detalle page. */
-router.get('/', function(req, res, next) {
-  res.render('productDetail', { title: 'Express' });
-});
-
-module.exports = router;
