6d6d55a21b256062dfdae98fda15517f5e3e7235
nachorm96 <rubio.nacho@yahoo.com.ar>
2024-01-01 17:56:40 -0300

    ruta de envio de productos

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ module.exports = router; */
 const express = require('express');
 const { index, admin, cart } = require('../controllers/indexController');
 const router = express.Router();
+const productsData = require('../data/productos.json');
 // const indexController = require('../controllers/indexController')
 
 /* /index */
@@ -20,5 +21,8 @@ const router = express.Router();
 router.get('/',index)
 router.get('/admin',admin)
 router.get('/carrito',cart)
+router.get('/', (req, res) => {
+    res.render('index', { products: productsData });  /* para enviar los productos a la vista */
+  });
 
 module.exports = router;
