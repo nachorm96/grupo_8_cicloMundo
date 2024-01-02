8b926dc6166803a06aeb21c9bac7b766036f1346
Carlos Garcia <corzuelachaco2019@gmail.com>
2024-01-01 21:25:44 -0300

    se agrega la funcion de agregar nuevo  producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,9 +1,38 @@
+const {existsSync, unlinkSync} = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+
 module.exports = {
     detail : (req,res)=> {
         return res.render('products/productDetail')
     },
     add : (req, res) => {
         return res.render('products/product-add')
+    },
+    store: (req, res) => {
+        const lastID = products[products.length - 1].id;
+		const {nombre, precio, rodado,color,categoria, descripcion,mainImg} = req.body;
+
+        const nuevoProduct = {
+            id:lastID + 1,
+            nombre:nombre,
+            precio:+precio,
+            rodado: Array.isArray(rodado) ? rodado : rodado.split(',').map(rodado => rodado.trim()),
+            color: Array.isArray(color) ? color : color.split(',').map(color => color.trim()),
+            categoria:categoria,
+            descripcion:descripcion,
+            mainImg:req.files.map(file => file.filename),
+        }
+        products.push(nuevoProduct);
+
+        fs.writeFileSync(productsFilePath,JSON.stringify(products),'utf-8')
+
+        res.redirect('/admin');
     },
     edit : (req, res) => {
         return res.render('products/product-edit')
