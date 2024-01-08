7de336ad6ba080689db5dfde63205007628b3a23
Carlos Garcia <corzuelachaco2019@gmail.com>
2024-01-08 14:14:07 -0300

    se borra codigo muerto

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const { index, admin, cart } = require('../controllers/indexController');
 const router = express.Router();
-const productsData = require('../data/productos.json');
+// const productsData = require('../data/productos.json');
 // const indexController = require('../controllers/indexController')
 
 
 router.get('/',index)
 router.get('/admin',admin)
 router.get('/carrito',cart)
-router.get('/', (req, res) => {
-    res.render('index', { products: productsData });  /* para enviar los productos a la vista */
-  });
 
 module.exports = router;
