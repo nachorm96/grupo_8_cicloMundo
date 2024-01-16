1fe767be9e9570c11c6c8e9582a5660055c41933
Carlos Garcia <corzuelachaco2019@gmail.com>
2024-01-15 23:19:48 -0300

    Implementar el registro de usuarios

--- src/controllers/usersControler.js
+++ src/controllers/usersControler.js
@@ -1,8 +1,34 @@
+const {existsSync, unlinkSync} = require('fs')
+const fs = require('fs');
+const path = require('path');
+const bcryptjs = require('bcryptjs')
+
+const productsFilePath = path.join(__dirname, '../data/usuarios.json');
+
 module.exports = {
     login : (req,res)=> {
         return res.render('users/login')
     },
     register : (req,res)=> {
         return res.render('users/register')
+    },
+    usuarioAdd : (req, res)=> {
+        const usuarios = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        const lastID = usuarios[usuarios.length - 1].id;
+		const {nombre, apellido, email, password,imgUser} = req.body;
+
+        const nuevoUsuario = {
+            id : lastID + 1,
+            nombre : nombre,
+            apellido : apellido,
+            email : email,
+            password : bcryptjs.hashSync(password.trim(),10),
+            imgUser : req.file ? req.file.filename : null
+        }
+        usuarios.push(nuevoUsuario);
+
+        fs.writeFileSync(productsFilePath,JSON.stringify(usuarios),'utf-8')
+
+        res.redirect('/users/login');
     }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
-const { login, register } = require('../controllers/usersControler');
+const { login, register, usuarioAdd } = require('../controllers/usersControler');
+const uploadUaer = require('../../middlewares/uploadUser');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/login',login)
       .get('/register',register)
+      .post('/register',uploadUaer.single('img-users'),usuarioAdd)
 
 module.exports = router;
